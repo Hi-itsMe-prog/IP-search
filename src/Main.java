f263739fe420ad4a2ac597400ac85d4ab84f923a
Mary <Anymail@mail.com>
2025-02-27 08:56:41 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Enter the randon string with IP-address: ");
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+
+        String ipRegex = "\\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b";
+        Pattern pattern = Pattern.compile(ipRegex);
+        Matcher matcher = pattern.matcher(s);
+
+        boolean found = false;
+
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+            found = true;
+        }
+
+        if (!found) {
+            System.out.println("IP address not found in the string.");
+        }
     }
 }
